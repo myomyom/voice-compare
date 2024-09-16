432f6fdf04fad8bf74e3f72e56c7d784b1a60682
myomyom <mayomayosine@gmail.com>
2024-09-16 12:12:19 +0200

    deploy

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/voice-compare/",
+  base: "https://myomyom.github.io/voice-compare/",
   plugins: [react()],
 });
